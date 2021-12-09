162104d36b7b7369a5a9fb4f834a05acedbce3cd
DR <DR>
2021-12-10 00:56:23 +0100

    update

--- main.py
+++ main.py
@@ -12,46 +12,121 @@ from re import search
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+import requests
+import re
 
 # Constants
-data_folder = './data'
+csv_data_folder = './data'
+covid_api_url = "https://api.covid19api.com/summary"
 
 
-# Getter for data from files. Just a small demonstration of using functions.
-def get_data(folder, name):
-    for file_name in Path(folder).glob("*.csv"):
+# REST API based on John Hopkins University COVID-19 dataset.
+response = requests.get(url=covid_api_url)
+
+# check for successfully request (status code 200)
+if response.status_code == 200:
+    result = response.json()
+
+# print(result)
+
+# get data for the countries into dataframe
+df = pd.json_normalize(result['Countries'])
+
+#print(df.info())
+
+# List of columns to delete
+del_columns = ['ID', 'CountryCode', 'Slug', 'NewRecovered', 'TotalRecovered']
+
+df.drop(del_columns, axis=1, inplace=True)
+
+# create an overview of covid19 cases worldwide
+#df.loc['Total'] = df.sum(numeric_only=True)
+
+# check whether the data date is the same in all countries.
+# convert date to datetime Dtype and change the date format to YYYY-MM-DD using lambda function.
+df['Date'] = pd.to_datetime(df['Date'])
+df['Date'] = df.Date.apply(lambda x: x.strftime('%Y-%m-%d')).astype('datetime64[ns]')
+
+print('different-date', df[df['Date'] == df.loc[0, ['Date']]])
+
+
+#print(df.head())
+
+
+
+
+
+
+
+
+
+# Getter for data from files.
+def get_data(name):
+    for file_name in Path(csv_data_folder).glob("*.csv"):
         if search(name, str(file_name)):
             df = pd.read_csv(file_name)
             return df
 
 
 # creating dataframes using function
-df_confirmed = get_data(data_folder, 'confirmed')
-df_deaths = get_data(data_folder, 'deaths')
-df_recovered = get_data(data_folder, 'recovered')
-df_vaccine = get_data(data_folder, 'vaccine')
+df_confirmed = get_data('confirmed')
+df_deaths = get_data('deaths')
+df_recovered = get_data('recovered')
+df_vaccine = get_data('vaccine')
 
 # checking the structure
-print(df_confirmed.head())
+# print(df_confirmed.head())
+# print(df_confirmed.head())
+
+#
+
+
+
+#
+df_vaccine.rename(columns={'Country_Region': 'Country/Region', 'Province_State': 'Province/State'}, inplace=True)
+df_confirmed.drop(columns=['Province/State', 'Lat', 'Long'], inplace=True)
 
 # creating new dataframe for Germany
-df_confirmed_de = df_confirmed[df_confirmed['Country'] == 'Germany']
-df_confirmed_ru = df_confirmed[df_confirmed['Country'] == 'Russia']
-df_confirmed_us = df_confirmed[df_confirmed['Country'] == 'US']
+df_confirmed_de = df_confirmed[df_confirmed['Country/Region'] == 'Germany']
+df_confirmed_ru = df_confirmed[df_confirmed['Country/Region'] == 'Russia']
+df_confirmed_us = df_confirmed[df_confirmed['Country/Region'] == 'US']
 
+# print('vaccine', df_vaccine.head())
+# print('death', df_deaths.head())
+# print('df_recovered', df_recovered.head())
+# print('conf', df_confirmed.head())
 
+# Todo: create a function for each dataset
+
+# merge countries into one dataframe
+merged_df = pd.concat([df_confirmed_de, df_confirmed_ru, df_confirmed_us])
+# rework
+
+test = merged_df.melt(id_vars=['Country'], var_name='Date', value_name='value')
+test2 = test.pivot(index='Date', columns='Country', values="value")
+
+test2.index = pd.to_datetime(test2.index)
+test2.sort_index(inplace=True)
+
+# Todo: End create function
+
+# sns.set()
+# plt.plot(test2)
+# plt.title('Confirmed cases')
+# plt.ylabel('Amount')
+# plt.yticks()
+# plt.show()
+# sns.set_theme(style="whitegrid")
+# sns.lineplot(data=test2, palette="tab10", linewidth=2.5)
+# plt.show()
 
 # transform the dataset and merge all together
-
-
-
-
 
 
 # Sources
 
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.transpose.html
-
 
 
 # df_confirmed = pd.read_csv('./data/time_series_covid19_confirmed_global.csv')
