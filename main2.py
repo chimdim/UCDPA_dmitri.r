ea7b264277f8ba6726af59b35012905e0d231f1b
DR <DR>
2021-12-18 13:57:26 +0100

    charts updates

--- main2.py
+++ main2.py
@@ -3,6 +3,9 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 # dataset source: https://www.kaggle.com/mrdaniilak/russia-real-estate-20182021
 # original dataset was removed from this project due large size
 
@@ -36,26 +39,40 @@ df_moscow['price'] = df_moscow['price'].abs()
 # removing unrealistic prices for properties that may be due to monthly rent
 df_moscow.drop(df_moscow[df_moscow['price'] < 1000000].index, inplace=True)
 
-# removing too expensive properties > 1.8 million €
-df_moscow.drop(df_moscow[df_moscow['price'] > 150000000].index, inplace=True)
+# removing too high prices
+df_moscow.drop(df_moscow[df_moscow['price'] > 300000000].index, inplace=True)
+
+# visualization of prices in the dataset
+plt.figure(figsize=(10,6))
+plt.hist(df_moscow['price'], bins=40)
+plt.show()
+
+# reduce max price to 65 million rubel which reflects most of the properties in dataset
+df_moscow.drop(df_moscow[df_moscow['price'] > 65000000].index, inplace=True)
+
+# display prices in millions
+df_moscow['price'] = df_moscow['price']/1000000
 
 # drop properties with less or equal rooms then 0
 df_moscow.drop(df_moscow[df_moscow['rooms'] < 0].index, inplace=True)
 
 # drop columns
-df_moscow.drop(['time', 'Unnamed: 0', 'region'], axis=1, inplace=True)
-
+drop_columns = ['time', 'Unnamed: 0', 'region']
+df_moscow.drop(columns=drop_columns, inplace=True)
 
 #print(df_moscow.corr())
-#print(df_moscow.info())
 
-
-
-sns.heatmap(df_moscow.corr(), cmap="YlGnBu" )
+fig,ax = plt.subplots(figsize=(12, 10))
+sns.heatmap(df_moscow.corr(), cmap="YlGnBu", linewidth=0.2, cbar_kws={"shrink": .6})
+ax.xaxis.tick_top()
+plt.xticks(rotation="30")
+ax.set_title('Correlation matrix: Real estate in Moscow', fontsize=18, pad=20)
 plt.show()
 
-# Charts
-fig, (ax1, ax2) = plt.subplots(2,1, figsize=(8, 8))
+
+plt.figure(figsize=(10,6))
+plt.hist(df_moscow['price'], bins=40)
+plt.show()
 
 
 
