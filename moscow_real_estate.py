08e59496885d5f018e21141a2f354119050dcd90
DR <DR>
2021-12-22 00:12:15 +0100

    ML changes

--- moscow_real_estate.py
+++ moscow_real_estate.py
@@ -1,12 +1,14 @@
 import pandas as pd
-from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
 
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn import svm
-from sklearn import linear_model
+from matplotlib import pyplot as plt
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.ensemble import GradientBoostingRegressor, HistGradientBoostingRegressor
+from xgboost import XGBRegressor
+
 
 
 # --- original dataset was removed from this project due large size ---
@@ -45,8 +47,8 @@ df_moscow.drop(df_moscow[df_moscow['price'] < 1000000].index, inplace=True)
 df_moscow.drop(df_moscow[df_moscow['price'] > 300000000].index, inplace=True)
 
 # visualization of prices in the dataset
-plt.figure(figsize=(10, 6))
-plt.hist(df_moscow['price'], bins=40)
+# plt.figure(figsize=(10, 6))
+# plt.hist(df_moscow['price'], bins=40)
 # plt.show()
 
 # reduce max price to 65 million rubles which reflects most of the properties in dataset
@@ -61,79 +63,100 @@ df_moscow.drop(df_moscow[df_moscow['rooms'] < 1].index, inplace=True)
 # drop columns
 drop_columns = ['time', 'Unnamed: 0', 'region', 'date']
 df_moscow.drop(columns=drop_columns, inplace=True)
+df_moscow.reset_index(drop=True, inplace=True)
 
-# Charts
-plt.figure(figsize=(10, 6))
-sns.histplot(df_moscow['price'])
-plt.xlabel('price (million rubles)')
-plt.ylabel('real estate objects')
+# Chart: Histogram
+# fig, ax = plt.subplots(figsize=(8, 6))
+# ax = sns.histplot(df_moscow['price'])
+# plt.xlabel('Price (million rubles)')
+# plt.ylabel('Real estate objects')
 # plt.show()
-
-# print(df_moscow.corr())
-
-fig, ax = plt.subplots(figsize=(9, 6))
-sns.heatmap(df_moscow.corr(), cmap="YlGnBu", linewidth=0.2, cbar_kws={"shrink": .6})
-ax.xaxis.tick_top()
-plt.xticks(rotation="30")
-ax.set_title('Correlation matrix: Real estate in Moscow', fontsize=18, pad=20)
-# plt.show()
-
-# print(df_moscow['price'].corr(df_moscow['area']))
-
-x = df_moscow.drop('price', axis=1)
-y = df_moscow['price']
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.01, random_state=10)
-
-# lin regression
-lin_regression = linear_model.LinearRegression()
-lin_regression.fit(X_train, y_train)
-
-y_pred = lin_regression.predict(X_test)
-
-print('training data:', lin_regression.score(X_train, y_train))
-print('test data:', lin_regression.score(X_test, y_test))
-print('intercept:', lin_regression.intercept_)
-print('coef_:', lin_regression.coef_)
-
-print(pd.DataFrame(data=lin_regression.coef_, index=X_train.columns, columns=['coef']))
-
-# Ridge
-ridge = linear_model.Ridge(alpha=0.9)
-ridge.fit(X_train, y_train)
-ridge_pred = ridge.predict(X_test)
-print('ridge', ridge.score(X_test, y_test))
-
-# Lasso
-plt.close()
-lasso = linear_model.Lasso(alpha=0.9)
-lasso.fit(X_train, y_train)
-print('lasso', lasso.score(X_test, y_test))
-# print( 'lasso coeff:', lasso.coef_)
-
-
-# GridSearchCV
-# params = {'kernel': ('linear', 'poly', 'rbf', 'sigmoid'), 'C': [1, 5, 10], 'degree': [3, 8], 'coef0': [0.01, 10, 0.5],
-#           'gamma': ('auto', 'scale')},
-# svr = svm.SVR()
+# plt.close()
 #
-# search = GridSearchCV(estimator=svr, param_grid=params, cv=3, n_jobs=-1, verbose=2)
-# search.fit(X_train, y_train)
-# print(search.cv_results_)
+# # Chart: Heatmap
+# fig, ax = plt.subplots(figsize=(8, 6))
+# ax = sns.heatmap(df_moscow.corr(), cmap="YlGnBu", linewidth=0.2, cbar_kws={"shrink": .6})
+# plt.xticks(rotation="-45")
+# ax.set_title('Correlation matrix', fontsize=18, pad=20)
+# fig.tight_layout()
+# plt.show()
 
-classifiers = [
-    linear_model.SGDRegressor(),
-    linear_model.ARDRegression(),
-    linear_model.PassiveAggressiveRegressor(),
-    # linear_model.TheilSenRegressor(),
-    linear_model.Lasso(),
-    linear_model.Ridge(),
-    linear_model.LinearRegression(),
-    # svm.SVR()
-]
+# Preparation for test and train data
+x = df_moscow.drop('price', axis=1).values
+y = df_moscow['price'].values.reshape(-1,1)
 
-for item in classifiers:
-    print(item)
-    clf = item
-    clf.fit(X_train, y_train)
-    print(clf.score(X_test, y_test),'\n')
+
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state=10)
+
+# Print function
+def print_results(model, name):
+    print(name + ' cv_results: ', model.cv_results_)
+    print(name + ' best_params: ', model.best_params_)
+    print(name + ' best score: ', model.best_score_)
+    print(name + '  best estimator: ', model.best_estimator_)
+
+
+linear = linear_model.LinearRegression()
+linear.fit(X_train, y_train)
+print(linear.score(X_test,y_test))
+
+lasso = linear_model.Lasso()
+lasso.fit(X_train, y_train)
+print(lasso.score(X_test,y_test))
+
+ridge = linear_model.Ridge()
+
+
+
+
+# # HistGradientBoostingRegressor
+# model = HistGradientBoostingRegressor(random_state=10)
+# params = {
+#     'max_iter': [10, 20, 50],
+#     'max_depth': [3, 10]
+# }
+# hgb = GridSearchCV(estimator=model, param_grid=params, cv=5, verbose=2)
+# hgb.fit(X_train, y_train)
+# print_results(hgb, 'HistGradientBoostingRegressor')
+#
+#
+# # XGBooster
+# model2 = XGBRegressor()
+# params2 = {
+#     'max_depth': [3, 10],
+#     'n_estimators': [50, 100],
+#     'gamma': [0.05, 0.1]
+# }
+# xgb = GridSearchCV(estimator=model2, param_grid=params2, cv=5, verbose=2)
+# xgb.fit(X_train, y_train)
+# print_results(xgb, 'XGBRegressor')
+
+# GradientBoostingRegressor
+# model3 = GradientBoostingRegressor(random_state=10)
+# params = {
+#     'n_estimators': [50, 100],
+#     'max_depth': [3, 10]
+# }
+# gb = GridSearchCV(estimator=model3, param_grid=params, cv=5, verbose=2)
+# gb.fit(X_train, y_train)
+# print_results(gb, 'GradientBoostingRegressor')
+
+
+# Best estimator
+# xg = XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#                   colsample_bynode=1, colsample_bytree=1, enable_categorical=False,
+#                   gamma=0.05, gpu_id=-1, importance_type=None,
+#                   interaction_constraints='', learning_rate=0.300000012,
+#                   max_delta_step=0, max_depth=10, min_child_weight=1,
+#                   monotone_constraints='()', n_estimators=100, n_jobs=8,
+#                   num_parallel_tree=1, predictor='auto', random_state=0, reg_alpha=0,
+#                   reg_lambda=1, scale_pos_weight=1, subsample=1, tree_method='exact',
+#                   validate_parameters=1, verbosity=None)
+#
+# xg.fit(X_train, y_train)
+# print(xg.score(X_test, y_test))
+# prediction = xg.predict(X_test)
+# print(pd.DataFrame({'col1':prediction, 'col2':y_test},).reset_index(drop=True).head(20))
+# pd.DataFrame({'col1':prediction, 'col2':y_test}).reset_index(drop=True).to_csv('test_prediction.csv')
+
