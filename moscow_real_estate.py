4e3e701c86f22e8ddf5be6e133b9997507a01077
DR <DR>
2021-12-22 03:36:15 +0100

    ML improvements

--- moscow_real_estate.py
+++ moscow_real_estate.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.ensemble import GradientBoostingRegressor, HistGradientBoostingRegressor
 from xgboost import XGBRegressor
@@ -70,6 +70,52 @@ drop_columns = ['time', 'Unnamed: 0', 'region', 'date']
 df.drop(columns=drop_columns, inplace=True)
 
 
+
+# Todo: create Dataframe for results. Results {'model': [Score, MAE, RMSE]}
+model_name = np.empty(0, dtype="str")
+model_mae = np.empty(0,dtype='float64')
+model_rmse = np.empty([1,6], dtype='float64')
+model_r2 = np.empty([1,6], dtype='float64')
+
+model_name = np.append(model_name, 'sas')
+model_name = np.append(model_name, 'sas2')
+model_mae = np.append(model_mae, 4)
+model_mae = np.append(model_mae, 2)
+model_r2 = np.append(model_r2, 8)
+model_r2 = np.append(model_r2, 3)
+model_rmse = np.append(model_rmse, 1)
+model_rmse = np.append(model_rmse, 6)
+
+
+
+x = np.arange(len(model_name))
+width = 0.35  # the width of the bars
+
+#fig, (ax1, ax2, ax3) = plt.subplots(3,1, figsize=(8, 10))
+# fig, (ax1, ax2) = plt.subplots(2,1, figsize=(8, 10))
+#
+# ax1.bar(x, model_mae, width, label='mae')
+# ax2.bar(x, model_r2, width, label='mae')
+#ax2.bar(x, model_rmse, width, label='rmse')
+#ax3.bar(x, model_r2, width, label='r2')
+
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+# ax1.set_ylabel('Scores')
+# ax1.set_title('Scores by group and gender')
+# ax1.set_xticks(x, model_name)
+# ax1.legend()
+
+
+#ax.bar_label(rects1, padding=3)
+#ax.bar_label(rects2, padding=3)
+#
+# fig.tight_layout()
+#
+# plt.show()
+
+
+
 #df.reset_index(drop=True, inplace=True)
 
 # Chart: Histogram
@@ -80,13 +126,13 @@ df.drop(columns=drop_columns, inplace=True)
 # plt.show()
 # plt.close()
 #
-# # Chart: Heatmap
-# fig, ax = plt.subplots(figsize=(8, 6))
-# ax = sns.heatmap(df.corr(), cmap="YlGnBu", linewidth=0.2, cbar_kws={"shrink": .6})
-# plt.xticks(rotation="-45")
-# ax.set_title('Correlation matrix', fontsize=18, pad=20)
-# fig.tight_layout()
-# plt.show()
+# Chart: Heatmap
+fig, ax = plt.subplots(figsize=(8, 6))
+ax = sns.heatmap(df.corr(), cmap="YlGnBu", linewidth=0.2, annot=True, cbar_kws={"shrink": .6})
+plt.xticks(rotation="45")
+ax.set_title('Correlation matrix', fontsize=18, pad=20)
+fig.tight_layout()
+plt.show()
 
 # Preparation for test and train data
 x = df.drop('price', axis=1).values
@@ -117,6 +163,8 @@ ridge.fit(X_train, y_train)
 print(ridge.score(X_test,y_test))
 y_pred = ridge.predict(X_test)
 
+print(pd.DataFrame(y_pred).describe())
+
 print(r2_score(y_test,y_pred), mean_squared_error(y_test,y_pred))
 
 
@@ -132,7 +180,7 @@ print(r2_score(y_test,y_pred), mean_squared_error(y_test,y_pred))
 #
 #
 # # XGBooster
-# model2 = XGBRegressor()
+# model2 = XGBRegressor(verbosity=2)
 # params2 = {
 #     'max_depth': [3, 10],
 #     'n_estimators': [50, 100],
@@ -153,27 +201,28 @@ print(r2_score(y_test,y_pred), mean_squared_error(y_test,y_pred))
 # print_results(gb, 'GradientBoostingRegressor')
 
 
-#Best estimator
-xg = XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-                  colsample_bynode=1, colsample_bytree=1, enable_categorical=False,
-                  gamma=0.05, gpu_id=-1, importance_type=None,
-                  interaction_constraints='', learning_rate=0.300000012,
-                  max_delta_step=0, max_depth=10, min_child_weight=1,
-                  monotone_constraints='()', n_estimators=100, n_jobs=8,
-                  num_parallel_tree=1, predictor='auto', random_state=0, reg_alpha=0,
-                  reg_lambda=1, scale_pos_weight=1, subsample=1, tree_method='exact',
-                  validate_parameters=1, verbosity=None)
+#Best estimator. Todo: Check why there are negative predicted values when gbtree or dart booster were used.
+# xg = XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#              colsample_bynode=1, colsample_bytree=1, enable_categorical=False,
+#              gamma=0.1, gpu_id=-1, importance_type=None,
+#              interaction_constraints='', learning_rate=0.300000012,
+#              max_delta_step=0, max_depth=10, min_child_weight=1,
+#              monotone_constraints='()', n_estimators=100, n_jobs=8,
+#              num_parallel_tree=1, predictor='auto', random_state=0, reg_alpha=0,
+#              reg_lambda=1, scale_pos_weight=1, subsample=1, tree_method='exact',
+#              validate_parameters=1)
+# xg.fit(X_train, y_train)
+# print(xg.score(X_test, y_test))
+# prediction = xg.predict(X_test)
+# #print(pd.DataFrame({'col1':prediction, 'col2':y_test},).reset_index(drop=True).head(20))
+# #pd.DataFrame({'col1':prediction, 'col2':y_test}).reset_index(drop=True).to_csv('test_prediction.csv')
+# print(r2_score(y_test,prediction).mean(), np.sqrt(mean_squared_error(y_test,prediction)).mean(), mean_squared_error(y_test,prediction) )
+# print(pd.DataFrame(prediction).describe())
 
-xg.fit(X_train, y_train)
-print(xg.score(X_test, y_test))
-prediction = xg.predict(X_test)
-#print(pd.DataFrame({'col1':prediction, 'col2':y_test},).reset_index(drop=True).head(20))
-#pd.DataFrame({'col1':prediction, 'col2':y_test}).reset_index(drop=True).to_csv('test_prediction.csv')
-print(r2_score(y_test,prediction), mean_squared_error(y_test,prediction))
 
 
-fig, (ax1, ax2) = plt.subplots(2,1, figsize=(8, 10))
-ax1 = sns.histplot(y_test, color='blue')
-ax2 = sns.histplot(prediction, color='red')
-plt.show()
-#
+# fig, (ax1, ax2) = plt.subplots(2,1, figsize=(8, 10))
+# ax1 = sns.histplot(y_test, color='blue')
+# ax2 = sns.histplot(prediction, color='red')
+# plt.show()
+# #
