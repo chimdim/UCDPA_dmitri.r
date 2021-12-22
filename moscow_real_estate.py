2124bc89f9e942193f058c4273f706bdef74638c
DR <DR>
2021-12-22 18:47:24 +0100

    ML changes

--- moscow_real_estate.py
+++ moscow_real_estate.py
@@ -9,8 +9,6 @@ from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.ensemble import GradientBoostingRegressor, HistGradientBoostingRegressor
 from xgboost import XGBRegressor
 
-
-
 # --- original dataset was removed from this project due large size ---
 # dataset source: https://www.kaggle.com/mrdaniilak/russia-real-estate-20182021
 
@@ -22,6 +20,12 @@ from xgboost import XGBRegressor
 # new_df['region'].unique()
 # test[test['region'].isin([3,81])].to_csv('data2/moscow3.csv')
 
+
+# Global vars for results
+model_name = np.empty(0, dtype="str")
+model_mae = np.empty(0, dtype='float64')
+model_rmse = np.empty(0, dtype='float64')
+model_r2 = np.empty(0, dtype='float64')
 
 # -- Export Moscow real estate dataset as CVS file for this project
 # test[test['region'] == 3].to_csv('data2/moscow.csv')
@@ -69,139 +73,111 @@ df.drop(df[(df['area'] / df['rooms']) < 9].index, inplace=True)
 drop_columns = ['time', 'Unnamed: 0', 'region', 'date']
 df.drop(columns=drop_columns, inplace=True)
 
-
-
-# Todo: create Dataframe for results. Results {'model': [Score, MAE, RMSE]}
-model_name = np.empty(0, dtype="str")
-model_mae = np.empty(0,dtype='float64')
-model_rmse = np.empty([1,6], dtype='float64')
-model_r2 = np.empty([1,6], dtype='float64')
-
-model_name = np.append(model_name, 'sas')
-model_name = np.append(model_name, 'sas2')
-model_mae = np.append(model_mae, 4)
-model_mae = np.append(model_mae, 2)
-model_r2 = np.append(model_r2, 8)
-model_r2 = np.append(model_r2, 3)
-model_rmse = np.append(model_rmse, 1)
-model_rmse = np.append(model_rmse, 6)
-
-
-
-x = np.arange(len(model_name))
-width = 0.35  # the width of the bars
-
-#fig, (ax1, ax2, ax3) = plt.subplots(3,1, figsize=(8, 10))
-# fig, (ax1, ax2) = plt.subplots(2,1, figsize=(8, 10))
-#
-# ax1.bar(x, model_mae, width, label='mae')
-# ax2.bar(x, model_r2, width, label='mae')
-#ax2.bar(x, model_rmse, width, label='rmse')
-#ax3.bar(x, model_r2, width, label='r2')
-
-
-# Add some text for labels, title and custom x-axis tick labels, etc.
-# ax1.set_ylabel('Scores')
-# ax1.set_title('Scores by group and gender')
-# ax1.set_xticks(x, model_name)
-# ax1.legend()
-
-
-#ax.bar_label(rects1, padding=3)
-#ax.bar_label(rects2, padding=3)
-#
-# fig.tight_layout()
-#
-# plt.show()
-
-
-
-#df.reset_index(drop=True, inplace=True)
+df.reset_index(drop=True, inplace=True)
 
 # Chart: Histogram
-# fig, ax = plt.subplots(figsize=(8, 6))
-# ax = sns.histplot(df['price'])
-# plt.xlabel('Price (million rubles)')
-# plt.ylabel('Real estate objects')
-# plt.show()
-# plt.close()
-#
-# Chart: Heatmap
-fig, ax = plt.subplots(figsize=(8, 6))
-ax = sns.heatmap(df.corr(), cmap="YlGnBu", linewidth=0.2, annot=True, cbar_kws={"shrink": .6})
-plt.xticks(rotation="45")
-ax.set_title('Correlation matrix', fontsize=18, pad=20)
-fig.tight_layout()
+fig = plt.subplots(figsize=(8, 6))
+sns.histplot(df['price'])
+plt.xlabel('Price (million rubles)')
+plt.ylabel('Real estate objects')
 plt.show()
+plt.close()
+
+# Chart: Heatmap
+plt.subplots(figsize=(8, 6))
+sns.heatmap(df.corr(), cmap="YlGnBu", linewidth=0.2, annot=True, cbar_kws={"shrink": .6})
+plt.xticks(rotation="45")
+plt.title('Correlation matrix', fontsize=18, pad=20)
+plt.show()
+plt.close()
 
 # Preparation for test and train data
 x = df.drop('price', axis=1).values
-y = df['price'].values.reshape(-1,1)
-
-
+y = df['price'].values
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=40)
 
-# Print function
-def print_results(model, name):
-    print(name + ' cv_results: ', model.cv_results_)
-    print(name + ' best_params: ', model.best_params_)
-    print(name + ' best score: ', model.best_score_)
-    print(name + '  best estimator: ', model.best_estimator_)
+
+# Print function for tests
+def print_results(mdl, name):
+    print(name + ' cv_results: ', mdl.cv_results_)
+    print(name + ' best_params: ', mdl.best_params_)
+    print(name + ' best score: ', mdl.best_score_)
+    print(name + '  best estimator: ', mdl.best_estimator_)
 
 
+# append results to global array var.
+def add_results(name, pred_val, true_val,):
+    global model_rmse, model_r2, model_name, model_mae
+    score = r2_score(true_val, pred_val)
+    rmse = np.sqrt(mean_squared_error(true_val, pred_val)).mean()
+    mae = mean_absolute_error(true_val, pred_val)
+
+    model_name = np.append(model_name, name)
+    model_mae = np.append(model_mae, mae)
+    model_r2 = np.append(model_r2, score)
+    model_rmse = np.append(model_rmse, rmse)
+
+
+# Linear Regression
 linear = LinearRegression()
 linear.fit(X_train, y_train)
-print(linear.score(X_test,y_test))
+pred_lin = linear.predict(X_test)
+add_results('Linear Reg.', pred_lin, y_test)
 
+# Lasso regression
 lasso = Lasso(alpha=1.0)
 lasso.fit(X_train, y_train)
-print(lasso.score(X_test,y_test))
+pred_lasso = lasso.predict(X_test)
+add_results('Lasso', pred_lasso, y_test)
 
+# Ridge regression
 ridge = Ridge(alpha=1.0)
 ridge.fit(X_train, y_train)
-print(ridge.score(X_test,y_test))
-y_pred = ridge.predict(X_test)
+pred_ridge = ridge.predict(X_test)
+add_results('Ridge', pred_ridge, y_test)
 
-print(pd.DataFrame(y_pred).describe())
-
-print(r2_score(y_test,y_pred), mean_squared_error(y_test,y_pred))
-
-
-# # HistGradientBoostingRegressor
-# model = HistGradientBoostingRegressor(random_state=10)
-# params = {
-#     'max_iter': [10, 20, 50],
-#     'max_depth': [3, 10]
-# }
-# hgb = GridSearchCV(estimator=model, param_grid=params, cv=5, verbose=2)
-# hgb.fit(X_train, y_train)
+# HistGradientBoostingRegressor
+model = HistGradientBoostingRegressor(random_state=10)
+params = {
+    'max_iter': [10, 20, 50],
+    'max_depth': [3, 10]
+}
+hgb = GridSearchCV(estimator=model, param_grid=params, cv=5, verbose=2)
+hgb.fit(X_train, y_train)
+pred_hgb = hgb.predict(X_test)
+add_results('HistGradientBoosting', pred_hgb, y_test)
 # print_results(hgb, 'HistGradientBoostingRegressor')
-#
-#
-# # XGBooster
-# model2 = XGBRegressor(verbosity=2)
-# params2 = {
-#     'max_depth': [3, 10],
-#     'n_estimators': [50, 100],
-#     'gamma': [0.05, 0.1]
-# }
-# xgb = GridSearchCV(estimator=model2, param_grid=params2, cv=5, verbose=2)
-# xgb.fit(X_train, y_train)
+
+
+# XGBooster
+model2 = XGBRegressor()
+params2 = {
+    'max_depth': [3, 10],
+    'n_estimators': [50, 100],
+    'gamma': [0.05, 0.1]
+}
+xgb = GridSearchCV(estimator=model2, param_grid=params2, cv=5, verbose=2)
+xgb.fit(X_train, y_train)
+pred_xgb = xgb.predict(X_test)
+add_results('XGBooster', pred_xgb, y_test)
 # print_results(xgb, 'XGBRegressor')
 
+
 # GradientBoostingRegressor
-# model3 = GradientBoostingRegressor(random_state=10)
-# params = {
-#     'n_estimators': [50, 100],
-#     'max_depth': [3, 10]
-# }
-# gb = GridSearchCV(estimator=model3, param_grid=params, cv=5, verbose=2)
-# gb.fit(X_train, y_train)
+model3 = GradientBoostingRegressor(random_state=10)
+params = {
+    'n_estimators': [50, 100],
+    'max_depth': [3, 10]
+}
+gb = GridSearchCV(estimator=model3, param_grid=params, cv=5, verbose=2)
+gb.fit(X_train, y_train)
+pred_gb = gb.predict(X_test)
+add_results('GradientBoosting', pred_gb, y_test)
 # print_results(gb, 'GradientBoostingRegressor')
 
 
-#Best estimator. Todo: Check why there are negative predicted values when gbtree or dart booster were used.
+# Best estimator. Todo: Check why there are negative predicted values when gbtree or dart booster were used.
 # xg = XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
 #              colsample_bynode=1, colsample_bytree=1, enable_categorical=False,
 #              gamma=0.1, gpu_id=-1, importance_type=None,
@@ -220,9 +196,26 @@ print(r2_score(y_test,y_pred), mean_squared_error(y_test,y_pred))
 # print(pd.DataFrame(prediction).describe())
 
 
-
-# fig, (ax1, ax2) = plt.subplots(2,1, figsize=(8, 10))
-# ax1 = sns.histplot(y_test, color='blue')
-# ax2 = sns.histplot(prediction, color='red')
-# plt.show()
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(12, 12))
+x_ax = np.arange(len(model_name))
+col_width = 0.6
+b1 = ax1.bar(x_ax, model_mae, col_width, alpha=.8)
+b2 = ax2.bar(x_ax, model_rmse, col_width, alpha=.65)
+b3 = ax3.bar(x_ax, model_r2, col_width, alpha=.45)
+ax1.get_shared_x_axes().join(ax1, ax2, ax3)
+ax1.set_title('Mean absolute error')
+ax2.set_title('Root Mean Square Error')
+ax3.set_title('Score')
+# # ax1.set_ylabel('Scores')
+# # ax1.set_title('Scores by group and gender')
+ax1.set_xticks(x_ax, model_name)
+# # ax1.legend()
+#
+#
+ax1.bar_label(b1, padding=-20)
+ax2.bar_label(b2, padding=-20)
+ax3.bar_label(b3, padding=-20)
 # #
+fig.tight_layout()
+plt.show()
+
